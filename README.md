0b82f77930e3186689ff7a8113fbe79c0db2e745
vignesh <vvignesh1011@gmail.com>
2025-05-22 08:17:23 +0530

    updating dist and readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 #install the package
-npm i github:vvignesh1011/tree-cli.git
+git clone https://github.com/vvignesh1011/tree-cli.git
+npm link
 
 #run the package
 tree-cli
+
+# print specific folder using -r / --root
+
+example tree-cli --root ./src
+
+# omit folders for print using -o / --omit
+
+example tree-cli --omit ./dist ./node_modules
--- dist/printFolder.js
+++ dist/printFolder.js
@@ -9,8 +9,25 @@ const path_1 = __importDefault(require("path"));
 const folderUtil_1 = require("./folderUtil");
 const program = new commander_1.Command();
 program.option("-r, --root <path>", "base/root folder");
+program.option("-o, --omit <path...>", "omit the folder or file");
 program.parse(process.argv);
 const options = program.opts();
 let baseDir = options.root ? path_1.default.resolve(options.root) : process.cwd();
+let omitFolders = options.omit;
 const rootFolder = (0, folderUtil_1.getFolderStructure)(baseDir);
+if (omitFolders) {
+    omitFolders.forEach((value) => {
+        let omit = path_1.default.resolve(value);
+        let targets = path_1.default.relative(baseDir, omit).split(path_1.default.sep);
+        let parentNode = rootFolder;
+        let targetNode;
+        for (let i = 0; i < targets.length; i++) {
+            if (targetNode)
+                parentNode = targetNode;
+            targetNode = parentNode === null || parentNode === void 0 ? void 0 : parentNode.children.find((child) => child.val == targets[i]);
+        }
+        if (parentNode && targetNode)
+            parentNode.removeChild(targetNode);
+    });
+}
 rootFolder.print();
